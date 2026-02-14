5ed87c35eb4dc13271c134034eb03310846b5881
Andrii-Mitko <andreymit123@gmail.com>
2026-02-14 09:46:10 +0200

    task-1 v-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,7 @@
-console.log(document.querySelector('#categories').children.length);
+const number = document.querySelector('#categories').children.length;
+console.log(`Namber of categories: ${number}`);
+const categ = document.querySelectorAll('h2');
+categ.forEach(i => {
+  console.log(`Category: ${i.textContent}`);
+ console.log(`Elements: ${i.nextElementSibling.children.length}`);
+});
