22feb7fa7b6fbb19799e1b5c890980ab0d49900f
Andrii-Mitko <andreymit123@gmail.com>
2026-02-14 17:40:01 +0200

    add v-1

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('name-input');
+const output = document.getElementById('name-output');
+input.addEventListener('input', () => {
+  const name = input.value.trim();
+  output.textContent = name === '' ? 'Anonymous' : name;
+});
