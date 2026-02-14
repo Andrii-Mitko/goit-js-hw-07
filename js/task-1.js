a99032e4f22f7fb41418a109cb1f71bcf2df41ab
Andrii-Mitko <andreymit123@gmail.com>
2026-02-14 13:02:05 +0200

    task-2 v-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 const categ = document.querySelectorAll('li.item');
-console.log(`Namber of categories: ${categ.length}`);
-categ.forEach(i => {
-  console.log(`Category: ${i.querySelector('h2').textContent}`);
- console.log(`Elements: ${i.querySelectorAll('ul li').length}`);
+console.log(`Number of categories: ${categ.length}`);
+categ.forEach(category => {
+  console.log(`Category: ${category.querySelector('h2').textContent}`);
+ console.log(`Elements: ${category.querySelectorAll('ul li').length}`);
 });
